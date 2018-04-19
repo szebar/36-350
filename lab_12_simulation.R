3bfaab8314dd420fbe45fe191c0780504eaa8b7b
szebar <38514144+szebar@users.noreply.github.com>
2018-04-19 11:25:18 -0400

    Created model_select
    Created the function model_select

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -2,3 +2,6 @@ generate_data = function(n,p) {
   return(list(covariates = matrix(data = rnorm(n*p), nrow = n, ncol = p), responses = rnorm(n)))
   }
 
+model_select = function(covariates, responses, cutoff) {
+  return(lm(responses ~ covariates[,which(covariates <= cutoff)]))
+         }
